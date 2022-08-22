bfc49611c4bd2e97e21df1496ca1649baa56ea20
gina <f1233772002@gmail.com>
2022-08-22 11:34:55 +0800

    建置後台主要功能項目頁面

---
+++ back/bot.php
@@ -0,0 +1,13 @@
+<h2 class="ct">編輯頁尾版權區</h2>
+<form action="?" method="post">
+<table class="all">
+    <tr>
+        <td class="tt ct">頁尾宣告內容</td>
+        <td class="pp"><input type="text" name="bot" id=""></td>
+    </tr>
+</table>
+<div class="ct">
+<input type="submit" value="編輯">
+<input type="reset" value="重置">
+</div>
+</form>
