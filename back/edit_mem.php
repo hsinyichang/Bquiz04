7bbcefd799ea899281056f87040c20b2e86d7932
gina <f1233772002@gmail.com>
2022-08-23 10:51:06 +0800

    完成編輯會員資料功能

--- back/edit_mem.php
+++ back/edit_mem.php
@@ -1,50 +1,50 @@
 <?php
-$id=$_GET['id'];
-$row=$Mem->find($id);
+$row=$Mem->find($_GET['id']);
 ?>
 <h2 class="ct">編輯會員資料</h2>
-<table class="all">
+<table class="all" id="editForm">
     <tr>
-        <td class="tt ct">帳號</td>
-        <td class="pp"><?=$row['acc']?></td>
+        <td class="ct tt">帳號</td>
+        <td class="pp"><?=$row['acc'];?></td>
     </tr>
     <tr>
-        <td class="tt ct">密碼</td>
-        <td class="pp"><?=str_repeat('*',strlen($row['pw']));?></td>
+        <td class="ct tt">密碼</td>
+        <td class="pp">
+            <?=str_repeat('*',strlen($row['pw']));?>
+        </td>
     </tr>
     <tr>
-        <td class="tt ct">累積交易額</td>
-        <td class="pp"></td>
+        <td class="ct tt">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name" value="<?=$row['name'];?>"></td>
     </tr>
     <tr>
-        <td class="tt ct">姓名</td>
-        <td class="pp"><input type="text" name="name" id="name" value="<?=$row['name']?>"></td>
+        <td class="ct tt">電子信箱</td>
+        <td class="pp"><input type="text" name="email" id="email" value="<?=$row['email'];?>"></td>
     </tr>
     <tr>
-        <td class="tt ct">電子信箱</td>
-        <td class="pp"><input type="text" name="email" id="email" value="<?=$row['email']?>"></td>
+        <td class="ct tt">地址</td>
+        <td class="pp"><input type="text" name="addr" id="addr" value="<?=$row['addr'];?>"></td>
     </tr>
     <tr>
-        <td class="tt ct">住址</td>
-        <td class="pp"><input type="text" name="addr" id="addr" value="<?=$row['addr']?>"></td>
-    </tr>
-    <tr>
-        <td class="tt ct">電話</td>
-        <td class="pp"><input type="text" name="tel" id="tel" value="<?=$row['tel']?>"></td>
+        <td class="ct tt">電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel" value="<?=$row['tel'];?>"></td>
     </tr>
 </table>
 <div class="ct">
-    <button onclick="edit('mem')">編輯</button>
-    <button onclick="loction.reload()">重置</button> <!--將畫面重整 資料就又是原本的，這裡不是清空-->
+    <button onclick="edit('mem',<?=$row['id'];?>)">編輯</button>
+    <button onclick="location.reload()">重置</button>
     <button onclick="location.href='?do=mem'">取消</button>
 </div>
-
 <script>
-    function edit(table,id){
-        let form=$('table input').serializeArray()
-        $.post("./api/edit.php",{table,form,id},(res)=>{
+
+function edit(table ,id){
+    let form=$("#editForm input").serializeArray()
+
+    $.post("./api/edit.php",{table,form,id},(res)=>{
+        //console.log(res)
         location.href=`?do=${table}`;
-        //location.href='?do=mem';
+
     })
-    }
+}
+
 </script>
