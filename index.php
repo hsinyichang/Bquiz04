333090b7e97119f682709a1c168517f89739a894
gina <f1233772002@gmail.com>
2022-08-26 10:27:06 +0800

    增加商品數量顯示

--- index.php
+++ index.php
@@ -54,15 +54,17 @@
                 </div>
                 <?php
                 $bigs = $Type->all(['parent' => 0]);  //可從api/type_list.php 複製過來
-                foreach ($bigs as $big) {                
+                foreach ($bigs as $big) {
+                    $count=$Goods->math('count','id',['sh'=>1,'big'=>$big['id']]);                
                     echo " <div class='ww'>";    //包住整個大分類+中分類
-                    echo "<a href='?type={$big['id']}'>{$big['name']}</a>";
+                    echo "<a href='?type={$big['id']}'>{$big['name']}($count)</a>";
                     
                         $mids = $Type->all(['parent' => $big['id']]);
                         if (!empty($mids)) {
                             foreach ($mids as $mid) {
+                                $count=$Goods->math('count','id',['sh'=>1,'mid'=>$mid['id']]);
                                 echo "<div class='s'>";   //這裡是中分類
-                                echo "    <a href='?type={$mid['id']}' style='background:lightgreen;color:darkgreen'>{$mid['name']}</a>";
+                                echo "    <a href='?type={$mid['id']}' style='background:lightgreen;color:darkgreen'>{$mid['name']}($count)</a>";
                                 echo "</div>";
                             }
                         }
